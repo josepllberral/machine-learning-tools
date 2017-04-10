7414253a7d04f77a03454f985956b4f58eab293f
Josep Lluís Berral García <josepllberral@gmail.com>
2017-04-10 11:08:46 +0200

    Fixed path. Package version ready

--- rrbm/R/zzz.R
+++ rrbm/R/zzz.R
@@ -3,7 +3,7 @@
      library.dynam("rrbm", pkg, lib);
 }
 
-.onLoad <- function(libname, pkgname)
+.onLoad <- function(libname = find.package("rrbm"), pkgname = "rrbm")
 {
 	library.dynam("rrbm", package = c("rrbm"), lib.loc = .libPaths());
 }
